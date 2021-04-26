30a1b7e22ea254c34ff6c394e799c50afa4d3059
Minh <hlminh2000@gmail.com>
2021-04-26 18:33:51 +0000

    Revert "✨ adds timeout to waiting script"
    This reverts commit a3ec4770cf5e370b817686e2cfc4ac79621c66a0.

--- wait_for_chain.sh
+++ wait_for_chain.sh
@@ -1,5 +1,3 @@
-timeout 60 bash <<EOT
-        while  [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9944)" != "400" ]; 
-		do sleep 5;
-	done
-EOT
+while  [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9944)" != "400" ]; 
+	do sleep 5; 
+done
