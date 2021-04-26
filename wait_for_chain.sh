a3ec4770cf5e370b817686e2cfc4ac79621c66a0
Minh <hlminh2000@gmail.com>
2021-04-26 18:26:20 +0000

    ✨ adds timeout to waiting script

--- wait_for_chain.sh
+++ wait_for_chain.sh
@@ -1,3 +1,5 @@
-while  [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9944)" != "400" ]; 
-	do sleep 5; 
-done
+timeout 60 bash <<EOT
+        while  [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9944)" != "400" ]; 
+		do sleep 5;
+	done
+EOT
